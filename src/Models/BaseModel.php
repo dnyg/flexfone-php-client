07e2e8d772668821c6a57af911a7afdd7288bb35
Daniel Nygaard <dn@ordrestyring.dk>
2018-04-12 20:51:19 +0200

    fix(basemodel): type hinting changed to stdClass

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -1,9 +1,11 @@
 <?php
 namespace Flexfone\Models;
 
+use stdClass;
+
 class BaseModel
 {
-    public function __construct(array $values)
+    public function __construct(stdClass $values)
     {
         foreach ($values as $key => $value) {
             $this->{$key} = $value;
